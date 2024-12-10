4d89a8ff75ca2c2a0f05e727ba781c7e94c555dc
Shivam Prajapati <128581121+ShivamDeadlocker@users.noreply.github.com>
2024-12-10 14:17:25 +0530

    from L 13
    till vid #13

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import './App.css';
+import { useState } from 'react';
+// import About from './components/About';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+
+
 function App() {
+ 
+  const [mode,setMode] = useState('light');
+  const [alert,setAlert] = useState(null);
+
+  const showAlert = (message,type)=> {
+    setAlert({
+
+      msg: message,
+      type: type
+
+    })
+    setTimeout(()=>{setAlert(null)},2000)
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light')
+    {
+      setMode('dark')
+      document.body.style.backgroundColor = '#1E3A5F'
+      showAlert("Dark mode enabled!!!","success")
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode enabled!!!","success")
+    }
+
+  }
   return (
 <>
 
-<Navbar title="ShiamProTextUtils" about="About Us"/>
+<Navbar title="ShiamProTextUtils" about="About Us" mode={mode} toggleMode={toggleMode}/>
+<div className="container">
+  <Alert alert={alert}/>
+<TextForm heading="Enter your text below to analyze" mode={mode} showAlert={showAlert}/> 
+{/* <About/> */}
+</div>
+
 {/* <Navbar/> */}
 
 </>
